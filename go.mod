a283b3b22cfd483039e5496646231cdcaadf1094
Fedir RYKHTIK <feodor.rykhtik@gmail.com>
2020-11-08 13:19:35 +0100

    Unique instance ID added via google/uuid

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/fedir/counter
 
 go 1.14
+
+require github.com/google/uuid v1.1.2
--- main.go
+++ main.go
@@ -4,16 +4,25 @@ import (
 	"fmt"
 	"net/http"
 	"time"
+
+	"github.com/google/uuid"
 )
 
 var count int64
 var t0 time.Time
+var instanceID string
+
+func generateInstanceID() string {
+	id := uuid.New()
+	return id.String()
+}
 
 // CounterHandler shows such statistics number of visits and the time since server start
 func CounterHandler(w http.ResponseWriter, r *http.Request) {
 	count++
 	t1 := time.Now()
-	fmt.Fprintf(w, "Time since server start %v.\n", t1.Sub(t0))
+	fmt.Fprintf(w, "Instance ID: %s\n", instanceID)
+	fmt.Fprintf(w, "Time since instance start: %v\n", t1.Sub(t0))
 	fmt.Fprintf(w, "Visits: %d\n", count)
 }
 
@@ -23,6 +32,7 @@ func NullHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	t0 = time.Now()
+	instanceID = generateInstanceID()
 	http.HandleFunc("/", CounterHandler)
 	http.HandleFunc("/favicon.ico", NullHandler)
 	http.ListenAndServe(":8080", nil)
